6c8b4845a1528a52757d59a5d0b4d28cbcd249a6
Alejandro <wds@disnot.com>
2021-05-03 23:12:27 +0800

    修改nav

--- themes/realwds/source/realwds/realwds.js
+++ themes/realwds/source/realwds/realwds.js
@@ -1,3 +1,30 @@
+"use strict";
+function _typeof(e) {
+  return (_typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function(e) {
+    return typeof e
+  }
+  : function(e) {
+    return e && "function" == typeof Symbol && e.constructor === Symbol && e !== Symbol.prototype ? "symbol" : typeof e
+  }
+  )(e)
+}
+
+var navFn = {
+  switchDarkMode: function() {
+    "light" == ("dark" === document.documentElement.getAttribute("data-theme") ? "dark" : "light") ? (activateDarkMode(),
+    saveToLocal.set("theme", "dark", 2),
+    void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)) : (activateLightMode(),
+    saveToLocal.set("theme", "light", 2),
+    void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)),
+    "function" == typeof utterancesTheme && utterancesTheme(),
+    "object" === ("undefined" == typeof FB ? "undefined" : _typeof(FB)) && window.loadFBComment(),
+    window.DISQUS && document.getElementById("disqus_thread").children.length && setTimeout(function() {
+      return window.disqusReset()
+    }, 200)
+  }
+}
+
+
 //滚动条自动隐藏
 var t1 = 0;
 var t2 = 0;
@@ -19,17 +46,8 @@ function isScrollEnd() {
   }
 }
 
-var navFn = {
-  switchDarkMode: function() {
-    "light" == ("dark" === document.documentElement.getAttribute("data-theme") ? "dark" : "light") ? (activateDarkMode(),
-    saveToLocal.set("theme", "dark", 2),
-    void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)) : (activateLightMode(),
-    saveToLocal.set("theme", "light", 2),
-    void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)),
-    "function" == typeof utterancesTheme && utterancesTheme(),
-    "object" === ("undefined" == typeof FB ? "undefined" : _typeof(FB)) && window.loadFBComment(),
-    window.DISQUS && document.getElementById("disqus_thread").children.length && setTimeout(function() {
-        return window.disqusReset()
-    }, 200)
-  }
-}
+var OriginTitile = document.title, titleTime;
+document.addEventListener("visibilitychange", function() {
+    document.hidden ? (document.title = "realwds's blog",
+    clearTimeout(titleTime)) : document.title = OriginTitile
+});
