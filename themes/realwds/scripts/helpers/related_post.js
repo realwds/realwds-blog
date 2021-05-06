03591794fdcfdf22d369aa71d7b6e2a0343b39f3
Alejandro <wds@disnot.com>
2021-05-06 13:35:06 +0800

    修改主题样式

--- themes/realwds/scripts/helpers/related_post.js
+++ themes/realwds/scripts/helpers/related_post.js
@@ -71,20 +71,20 @@ hexo.extend.helper.register('related_posts', function (currentPost, allPosts) {
         lazySrc +
         '="' +
         this.url_for(cover) +
-        '" alt="cover">'
-      if (dateType === 'created') {
-        result +=
-          '<div class="content is-center"><div class="date"><i class="far fa-calendar-alt fa-fw"></i>' +
-          ' ' +
-          this.date(relatedPosts[i].created, hexoConfig.date_format) +
-          '</div>'
-      } else {
-        result +=
-          '<div class="content is-center"><div class="date"><i class="fas fa-history fa-fw"></i>' +
-          ' ' +
-          this.date(relatedPosts[i].updated, hexoConfig.date_format) +
-          '</div>'
-      }
+        '" alt="cover"><div class="content is-center">'
+      // if (dateType === 'created') {
+      //   result +=
+      //     '<div class="content is-center"><div class="date"><i class="far fa-calendar-alt fa-fw"></i>' +
+      //     ' ' +
+      //     this.date(relatedPosts[i].created, hexoConfig.date_format) +
+      //     '</div>'
+      // } else {
+      //   result +=
+      //     '<div class="content is-center"><div class="date"><i class="fas fa-history fa-fw"></i>' +
+      //     ' ' +
+      //     this.date(relatedPosts[i].updated, hexoConfig.date_format) +
+      //     '</div>'
+      // }
       result +=
         '<div class="title">' + relatedPosts[i].title + '</div>'
       result += '</div></a></div>'
